722760fbda2770d18e124d8b31318290dcdecba1
subratasikdar <subrata.sikdar.freelancer@gmail.com>
2023-05-11 22:54:14 +0530

    binary tree traversal insertion binary search

---
+++ tree/src/Node.java
@@ -0,0 +1,13 @@
+/**
+ * <code>Node</code> representing node of a tree
+ */
+public class Node {
+
+    int iData;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.iData = data;
+    }
+}
